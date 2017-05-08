c2a29ba06944e6eae25625eaaea152b55c511bea
Tu Ngo <tungo8810@gmail.com>
2017-05-08 13:52:16 -0700

    Create App and Root components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Todo Redux</h1>
+  );
+};
+
+export default App;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
 
+import configureStore from './store/store';
 import allTodos from './reducers/selectors';
 
-const store = configureStore();
-window.store = store;
+import Root from './components/root';
 
-window.allTodos = allTodos(store.getState());
+const store = configureStore();
 
 document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById("content");
-  ReactDOM.render(<h1>ToDos App</h1>, content);
+  ReactDOM.render(<Root store={store}/>, content);
 });
