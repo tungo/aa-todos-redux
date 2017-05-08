c1b040fe8eb1a3f6104601bbb62d95c70602cd4d
Tu Ngo <tungo8810@gmail.com>
2017-05-08 10:52:54 -0700

    Setup react to render h1

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const content = document.getElementById("content");
+  ReactDOM.render(<h1>ToDos App</h1>, content);
+});
