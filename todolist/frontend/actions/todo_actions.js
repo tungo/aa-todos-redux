70c801a5a1c563be6f841440f9108f42c6442e30
Tu Ngo <tungo8810@gmail.com>
2017-05-09 12:15:19 -0700

    Create asynchronous action creator fetch todos

--- todolist/frontend/actions/todo_actions.js
+++ todolist/frontend/actions/todo_actions.js
@@ -2,6 +2,8 @@ export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 
+import { requestTodos } from '../util/todo_api_util';
+
 export const receiveTodos = (todos) => {
   return {
     type: RECEIVE_TODOS,
@@ -21,4 +23,8 @@ export const removeTodo = (id) => {
     type: REMOVE_TODO,
     id
   };
+};
+
+export const fetchTodos = () => dispatch => {
+  requestTodos().then((response) => dispatch(receiveTodos(response)));
 };
